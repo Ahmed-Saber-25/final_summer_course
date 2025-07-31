046e588c0006b289255f7f10106bda97ff4673d6
ahmed saber <ahmed.saber@asgatech.local>
2025-07-31 19:46:54 +0300

    ## init project setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,6 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -51,6 +50,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/iti/myapplicationbnv/final_project/views/product_list/ProductsVM.kt
@@ -0,0 +1,4 @@
+package com.iti.myapplicationbnv.final_project.views.product_list
+
+class ProductsVM {
+}
