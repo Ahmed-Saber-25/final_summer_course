c37d88b7d855026ea5f87ce35892942e3a2c0e59
TassneemAmer <tassnemamer.2003@gmail.com>
2025-08-02 18:46:22 +0300

    Initial Android project with Empty Views Activity

--- app/src/main/java/com/example/recipe_app/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/recipe_app/ui/auth/LoginFragment.kt
@@ -1,4 +1,28 @@
 package com.example.recipe_app.ui.auth
 
-class LoginFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.recipe_app.R
+
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val signUpTextView = view.findViewById<TextView>(R.id.textView2)
+        signUpTextView.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+    }
 }
